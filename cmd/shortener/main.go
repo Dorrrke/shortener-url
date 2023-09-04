91302f82b39cfe462b14b3d818cf9041108296e3
Mikhail <dorrrke@yandex.ru>
2023-09-05 02:48:58 +0300

    First increment is completed

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,53 @@
 package main
 
-func main() {}
+import (
+	"io"
+	"net/http"
+	"strings"
+
+	"github.com/google/uuid"
+)
+
+var urlMap map[string]string
+
+func shortenerUrl(res http.ResponseWriter, req *http.Request) {
+	if req.Method == http.MethodPost {
+		body, err := io.ReadAll(req.Body)
+		if err != nil {
+			http.Error(res, err.Error(), 500)
+			return
+		}
+
+		urlId := strings.Split(uuid.New().String(), "-")[0]
+		urlMap[urlId] = string(body)
+		resultUrl := "http://localhost:8080/" + urlId
+		res.Header().Set("content-type", "text/plain")
+		res.WriteHeader(http.StatusCreated)
+		res.Write([]byte(resultUrl))
+
+	} else {
+		if req.Method == http.MethodGet {
+			urlId := strings.Split(req.URL.String(), "/")[1]
+			if _, ok := urlMap[urlId]; ok {
+				res.WriteHeader(http.StatusTemporaryRedirect)
+				res.Write([]byte(urlMap[urlId]))
+			}
+		} else {
+			res.WriteHeader(http.StatusBadRequest)
+		}
+	}
+
+}
+
+func main() {
+
+	urlMap = make(map[string]string)
+
+	mux := http.NewServeMux()
+	mux.HandleFunc(`/`, shortenerUrl)
+
+	err := http.ListenAndServe(`:8080`, mux)
+	if err != nil {
+		panic(err)
+	}
+}
