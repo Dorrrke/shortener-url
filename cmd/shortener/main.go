adf1d04da5051cfe61e5a718ab2375f1ba02617b
Mikhail <dorrrke@yandex.ru>
2023-09-05 22:54:54 +0300

    First increment fix#2

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -10,25 +10,25 @@ import (
 
 var urlMap map[string]string
 
-func shortenerUrl(res http.ResponseWriter, req *http.Request) {
+func shortenerURL(res http.ResponseWriter, req *http.Request) {
 	if req.Method == http.MethodPost {
 		body, err := io.ReadAll(req.Body)
 		if err != nil {
 			http.Error(res, err.Error(), 500)
 			return
 		}
-		urlId := strings.Split(uuid.New().String(), "-")[0]
-		urlMap[urlId] = string(body)
-		resultUrl := "http://localhost:8080/" + urlId
+		urlID := strings.Split(uuid.New().String(), "-")[0]
+		urlMap[urlID] = string(body)
+		resultURL := "http://localhost:8080/" + urlID
 		res.Header().Set("content-type", "text/plain")
 		res.WriteHeader(http.StatusCreated)
-		res.Write([]byte(resultUrl))
+		res.Write([]byte(resultURL))
 		return
 	} else {
 		if req.Method == http.MethodGet {
-			urlId := strings.Split(req.URL.String(), "/")[1]
-			if _, ok := urlMap[urlId]; ok {
-				url := urlMap[urlId]
+			urlID := strings.Split(req.URL.String(), "/")[1]
+			if _, ok := urlMap[urlID]; ok {
+				url := urlMap[urlID]
 				res.Header().Add("Location", url)
 				res.WriteHeader(http.StatusTemporaryRedirect)
 			}
@@ -44,7 +44,7 @@ func main() {
 	urlMap = make(map[string]string)
 
 	mux := http.NewServeMux()
-	mux.HandleFunc(`/`, shortenerUrl)
+	mux.HandleFunc(`/`, shortenerURL)
 
 	err := http.ListenAndServe(`:8080`, mux)
 	if err != nil {
